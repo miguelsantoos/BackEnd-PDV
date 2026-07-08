52d2f0dabe14bc134b0c05bd2200a87cde9aaa04
miguelsantoos <miguel3.santos3@gmail.com>
2026-07-08 19:05:57 -0300

    config

---
+++ src/main/java/com/cadastro/miguel/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.cadastro.miguel.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+           
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "https://front-end-pdv.vercel.app/"
+                        )
+                        .allowedMethods("*");
+                    }
+        };
+    }
+    
+}
