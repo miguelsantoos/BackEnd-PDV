2a3c488d0c03858cef6fd50f0502a4f48b30c770
miguelsantoos <miguel3.santos3@gmail.com>
2026-07-09 12:55:34 -0300

    Mudança no response

--- src/main/java/com/cadastro/miguel/controller/ClienteController.java
+++ src/main/java/com/cadastro/miguel/controller/ClienteController.java
@@ -1,8 +1,12 @@
 package com.cadastro.miguel.controller;
 
+import java.net.URI;
+import java.rmi.server.ServerCloneException;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cadastro.miguel.dto.ClienteRequest;
 import com.cadastro.miguel.dto.ClienteResponse;
@@ -30,8 +35,15 @@ public class ClienteController {
 
     // Recebe as informações do cliente
     @PostMapping
-    public ClienteResponse cadastrar(@RequestBody ClienteRequest clienteRequest) {
-        return clienteService.cadastrar(clienteRequest);
+    public ResponseEntity<ClienteResponse> cadastrar(@RequestBody ClienteRequest clienteRequest) {
+
+        URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(clienteRequest.nome())
+                        .toUri();  
+        ClienteResponse clienteResponse = clienteService.cadastrar(clienteRequest);
+        return ResponseEntity.created(location).body(clienteResponse);
     }
 
     // Recebe a chamada para buscar todos os clientes
