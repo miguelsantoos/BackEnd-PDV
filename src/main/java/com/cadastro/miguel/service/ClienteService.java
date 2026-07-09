201f3fc86b2bb483c11c7b08c05b3c251bbff808
miguelsantoos <miguel3.santos3@gmail.com>
2026-07-09 11:55:16 -0300

    Metodo get por id

--- src/main/java/com/cadastro/miguel/service/ClienteService.java
+++ src/main/java/com/cadastro/miguel/service/ClienteService.java
@@ -1,6 +1,7 @@
 package com.cadastro.miguel.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,11 @@ public class ClienteService {
     // Busca todos os clientes cadastrados
     public List<Cliente> buscarTodos() {
         return clienteRepository.findAll();
+    }
+
+    // Busca o cliente por id
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
     }
 
     // Deleta todos do banco de dados
